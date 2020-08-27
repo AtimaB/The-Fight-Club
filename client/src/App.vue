ddce1837c6211c9b0da48e5ef4765895839661a1
Collin <collinhodgs@gmail.com>
2020-08-27 12:09:39 -0600

    Added vue files and began working on them

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <The Fight Club />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/FightGame.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+</style>
