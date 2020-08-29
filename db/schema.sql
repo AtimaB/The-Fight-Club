56019654b531c7ff4e0203d3bdba27d8af5fea88
Collin <collinhodgs@gmail.com>
2020-08-29 12:06:34 -0600

    updating database and beggining to save input data to database

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,17 @@
-DROP DATABASE IF EXISTS ;
-CREATE DATABASE ;
+DROP DATABASE IF EXISTS fightclub;
 
+CREATE DATABASE fightclub;
+
+USE fightclub;
+
+CREATE TABLE player1 (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE player2 (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
-  var Todo = sequelize.define("Todo", {
+  var fightclub = sequelize.define("fightclub", {
     text: DataTypes.STRING,
     complete: DataTypes.BOOLEAN,
   });
-  return Todo;
+  return fightclub;
 };
