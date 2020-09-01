06d3ad778afb31917e9af70b00bca7d9af0b9fff
AtimaB <nickzehn10@hotmail.com>
2020-08-31 21:44:02 -0600

    recreated table in models

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,3 @@ CREATE DATABASE fightclub;
 
 USE fightclub;
 
-CREATE TABLE player1 (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE player2 (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-);
--- models/todo.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-  var fightclub = sequelize.define("fightclub", {
-    text: DataTypes.STRING,
-    complete: DataTypes.BOOLEAN,
-  });
-  return fightclub;
-};
