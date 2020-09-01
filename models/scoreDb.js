dc6115765c2e75911e0fe8e63e2647bdd5bfd814
AtimaB <nickzehn10@hotmail.com>
2020-09-01 12:48:33 -0600

    recreated tables

---
+++ models/scoreDb.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes){
+    var Score = sequelize.define("Score", {
+        score: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        damage: {
+            type: DataTypes.INTEGER, 
+            allowNull: false      
+        },
+        life: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    });
+    Score.associate = function(models) {
+        Score.belongsTo(models.playerDb, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Score;
+    }
